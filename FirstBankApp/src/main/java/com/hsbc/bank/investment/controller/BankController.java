dae44340fb01be72dbf16c8e6323c52cfeef3aad
truesparksoftware@gmail.com <jeevanbarguje1@gmail.com>
2022-10-29 20:22:50 +0530

    added basic code

---
+++ FirstBankApp/src/main/java/com/hsbc/bank/investment/controller/BankController.java
@@ -0,0 +1,27 @@
+package com.hsbc.bank.investment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bank")
+public class BankController {
+
+	
+	@GetMapping("/getbank")
+	public String getBank() {
+		String message="welcome to spring boot community";
+		return message;
+	}
+	
+	@GetMapping("/getAccount")
+	public String getBankAccount() {
+		String message="welcome to bank account";
+		return message;
+	}
+	
+	
+	
+	
+}
